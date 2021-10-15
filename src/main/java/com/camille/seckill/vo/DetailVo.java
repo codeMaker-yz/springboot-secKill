2d180e3536a07a3e78f7b69e22230f54a8884269
codemaker <582072822@qq.com>
2021-10-15 21:47:44 +0800

    实现商品详情页面静态化

---
+++ src/main/java/com/camille/seckill/vo/DetailVo.java
@@ -0,0 +1,16 @@
+package com.camille.seckill.vo;
+
+import com.camille.seckill.pojo.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailVo {
+    private User user;
+    private GoodsVo goodsVo;
+    private int secKillStatus;
+    private long remainSeconds;
+}
